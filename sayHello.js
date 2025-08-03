e0f988c820cddc53adf02e39b00c54d78e249fef
ann <ann.foventures@gmail.com>
2025-08-03 22:11:26 +0200

    cleaned up the code

--- sayHello.js
+++ sayHello.js
@@ -1,6 +1,4 @@
-// Some comment change for rebase
 function sayHello(name, surname) {
     console.log('Hello ${name} ${surname}');
 }
 sayHello('Robot', 'Dreams');
-// This code exports a function that logs a greeting message to the console.
