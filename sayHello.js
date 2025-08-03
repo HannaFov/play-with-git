c413a67680ac3266d8946968d88dcac1a1ffef12
ann <ann.foventures@gmail.com>
2025-08-03 14:58:47 +0200

    changed sayHello function

--- sayHello.js
+++ sayHello.js
@@ -1,3 +1,5 @@
-export function sayHello() {
-    console.log("Hello, World!");
+export function sayHello(name, surname) {
+    console.log('${name} ${surname}');
 }
+sayHello('Robot', 'Dreams');
+// This code exports a function that logs a greeting message to the console.
